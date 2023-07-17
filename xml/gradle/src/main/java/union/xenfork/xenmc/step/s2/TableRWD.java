f9c0bd5aa8117bf69d1c8bc3ed3f5331388cfb31
baka4n <qq474899581@gmail.com>
2023-07-17 11:59:23 +0800

    package doc

--- xml/gradle/src/main/java/union/xenfork/xenmc/step/s2/TableRWD.java
+++ xml/gradle/src/main/java/union/xenfork/xenmc/step/s2/TableRWD.java
@@ -6,9 +6,7 @@ import cn.hutool.core.text.csv.CsvWriter;
 import cn.hutool.core.util.XmlUtil;
 import cn.hutool.http.HttpUtil;
 import org.gradle.api.Project;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Text;
+import org.w3c.dom.*;
 import union.xenfork.xenmc.extensions.MinecraftExtension;
 import union.xenfork.xenmc.gradle.BootstrappedPluginProject;
 import union.xenfork.xenmc.gradle.Utils;
@@ -16,6 +14,7 @@ import union.xenfork.xenmc.read.ClassReader;
 import union.xenfork.xenmc.read.MappingTable;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.StandardCopyOption;
 import java.util.*;
 
@@ -27,17 +26,37 @@ public class TableRWD implements BootstrappedPluginProject {
         if (minecraft.xenmc.remapTypesDir == null) {
             minecraft.xenmc.remapTypesDir = new File(minecraft.xenmc.cacheHome, "remapof" + separator + minecraft.version);
         }
+        String NAME_LINE = "^.+:";
         Utils.setupMessagePrefix(project, minecraft);
 //        new ClassReader(MinecraftExtension.clientGame.toPath(), new File(MinecraftExtension.game, "client").toPath()).cfr();
         if (minecraft.xenmc.remapTypesOf.equals("xml")) {
             File file = new File(minecraft.xenmc.remapTypesDir, "xenmc.xml");
+            List<String> lines = FileUtil.readLines(MinecraftExtension.clientMapping, StandardCharsets.UTF_8);
             if (minecraft.xenmc.getIsLoader()) {
                 if (!file.exists()) {
                     Document xml = XmlUtil.createXml();
                     Element element = xml.createElement("data");
                     element.setAttribute("version", minecraft.version + "-" + LocalDateTimeUtil.now());
+
+                    List<String> strings = new ArrayList<>();
+                    for (final String line : lines) {
+                        if (line.matches(NAME_LINE)) {
+                            final String s = line.replace(":", "").split("->")[0].trim();
+                            if (strings.stream().filter(s1 -> s1.equals(s.substring(0, s.lastIndexOf(".")))).toList().isEmpty()) {
+                                strings.add(s.substring(0, s.lastIndexOf(".")));
+                            }
+                        }
+                    }
                     Element element1 = xml.createElement("packages");
-                    element1.setAttribute("name", "a");
+                    for (String string : strings) {
+                        Element element2 = xml.createElement("name");
+                        element2.setAttribute("id", string);
+                        Element element3 = xml.createElement("javadoc");
+                        element3.setAttribute("_0", "");
+                        element3.setAttribute("_1", "");
+                        element2.appendChild(element3);
+                        element1.appendChild(element2);
+                    }
                     element.appendChild(element1);
                     xml.appendChild(element);
                     XmlUtil.toFile(xml, String.valueOf(file));
